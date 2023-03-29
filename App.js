5b041b87b88462524cd4c016acaa4381c535ad2f
Emir <emir.azimov@gmail.com>
2023-03-29 17:59:22 +0600

    delegated post component

--- App.js
+++ App.js
@@ -1,55 +1,16 @@
 import { StatusBar } from "expo-status-bar"
 import { Image, Text, View } from "react-native"
 import styled from "styled-components/native"
-
-const Post = styled.View`
-  flex-direction: row;
-  /* flex: 1; */
-  padding: 15px;
-  /* background-color: red; */
-  height: auto;
-  /* height: 100px;
-  width: 100px; */
-  /* border-radius: 30px; */
-  margin-top: 16%;
-  border-bottom-width: 1px;
-  border-bottom-color: rgba(0, 0, 0, 0.1);
-  border-bottom-style: solid;
-`
-
-const PostImage = styled.Image`
-  width: 60px;
-  height: 60px;
-  border-radius: 50%;
-  margin-right: 12px;
-`
-const PostTitle = styled.Text`
-  font-size: 17px;
-  font-weight: 700;
-`
-const PostDetails = styled.View``
-
-const PostDate = styled.Text`
-  font-size: 12px;
-  color: rgba(0, 0, 0, 0.4);
-  margin-top: 2px;
-`
+import { Post } from "./components/Post"
 
 export default function App() {
   return (
     <View>
-      <Post>
-        <PostImage
-          source={{
-            uri: "https://landing-page-nextjs.s3.us-east-2.amazonaws.com/logo.png",
-          }}
-        />
-        <PostDetails>
-          <PostTitle>Тестовая статья</PostTitle>
-          <PostDate>20/12/2022</PostDate>
-        </PostDetails>
-      </Post>
-
+      <Post
+        title="Тест"
+        createdAt="20/12/22"
+        imageUri="https://landing-page-nextjs.s3.us-east-2.amazonaws.com/logo.png"
+      />
       <StatusBar theme="auto" />
     </View>
   )
---
+++ components/Post.jsx
@@ -0,0 +1,58 @@
+import { StatusBar } from "expo-status-bar"
+import { Image, Text, View } from "react-native"
+import styled from "styled-components/native"
+
+const PostView = styled.View`
+  flex-direction: row;
+  /* flex: 1; */
+  padding: 15px;
+  /* background-color: red; */
+  height: auto;
+  /* height: 100px;
+  width: 100px; */
+  /* border-radius: 30px; */
+  margin-top: 16%;
+  border-bottom-width: 1px;
+  border-bottom-color: rgba(0, 0, 0, 0.1);
+  border-bottom-style: solid;
+`
+
+const PostImage = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  margin-right: 12px;
+`
+const PostTitle = styled.Text`
+  font-size: 17px;
+  font-weight: 700;
+`
+const PostDetails = styled.View`
+  flex-direction: column;
+  flex-grow: 1;
+  justify-content: center;
+`
+
+const PostDate = styled.Text`
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.4);
+  margin-top: 2px;
+`
+
+export const Post = ({ title, imageUrl, createdAt }) => {
+  return (
+    <PostView>
+      <PostImage
+        source={{
+          uri: imageUrl,
+        }}
+      />
+      <PostDetails></PostDetails>
+        <PostTitle>{title}</PostTitle>
+        <PostDate>{createdAt}</PostDate>
+      </PostDetails>
+    </PostView>
+
+    // <StatusBar theme="auto" />
+  )
+}
