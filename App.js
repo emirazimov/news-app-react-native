2b9535a87be30b67dfda6fcfda71140cd87cc9cb
Emir <emir.azimov@gmail.com>
2023-03-29 17:47:44 +0600

    last updates

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { Image, Text, View } from "react-native"
+import styled from "styled-components/native"
+
+const Post = styled.View`
+  flex-direction: row;
+  /* flex: 1; */
+  padding: 15px;
+  /* background-color: red; */
+  height: auto;
+  /* height: 100px;
+  width: 100px; */
+  /* border-radius: 30px; */
+  margin-top: 16%;
+  border-bottom-width: 1px;
+  border-bottom-color: rgba(0, 0, 0, 0.1);
+  border-bottom-style: solid;
+`
+
+const PostImage = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  margin-right: 12px;
+`
+const PostTitle = styled.Text`
+  font-size: 17px;
+  font-weight: 700;
+`
+const PostDetails = styled.View``
+
+const PostDate = styled.Text`
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.4);
+  margin-top: 2px;
+`
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Post>
+        <PostImage
+          source={{
+            uri: "https://landing-page-nextjs.s3.us-east-2.amazonaws.com/logo.png",
+          }}
+        />
+        <PostDetails>
+          <PostTitle>Тестовая статья</PostTitle>
+          <PostDate>20/12/2022</PostDate>
+        </PostDetails>
+      </Post>
+
+      <StatusBar theme="auto" />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
